7d7dce27b415e98f9ff63e61ddae2fd3c4129291
Bishal Prasad <biprasad@microsoft.com>
2016-09-26 14:25:56 +0530

    Change delete logic to delete oldest published version if there are more than 4 versions.
    Modified gulpfile to package UI zip also as part of build task

--- CDScripts/DeleteOldExtension.ps1
+++ CDScripts/DeleteOldExtension.ps1
@@ -17,12 +17,27 @@ param(
     [string]$versionToDelete
 )
 
-if($versionToDelete -eq "WILL_BET_SET_AT_RUNTIME")
+if(($versionToDelete -eq "WILL_BET_SET_AT_RUNTIME") -or ($versionToDelete -eq "") -or ($versionToDelete -eq $null))
 {
-    return
+    # Fetching list of published extension handler. Using South Central US location. Since, we are insterested in oldest version, it does not matter which location we use as replication would have anyways completed.
+    $extensions = Get-AzureRmVMExtensionImage -Location southcentralus -PublisherName $publisher -Type $extensionName
+
+    Write-Host "Published extension handler versions:"
+    $extensions | % { Write-Host $_.version}
+
+    if($extensions.Count -ge 5) 
+    {
+        $versionToDelete = $extensions[0].Version
+        Write-Host "5 or more extension handler versions are published. Oldest version will be deleted"
+    }
+    else
+    {
+        Write-Host "Less than 5 handler version published currently. None will be deleted"
+        exit 0
+    }
 }
 
-Write-Host "Will delete extension version: $versionToDelete"
+Write-Host "Deleting extension version: $versionToDelete"
 
 $subscription = Get-AzureSubscription -Current –ExtendedDetails
 $subscription.Certificate.Thumbprint

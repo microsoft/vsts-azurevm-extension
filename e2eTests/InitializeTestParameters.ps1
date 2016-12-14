e2a9849bbacd0c50c0089f5af59e73406a8e2ac2
Bishal Prasad <biprasad@microsoft.com>
2016-12-14 14:30:29 +0530

    linux e2e cdp scripts changes

--- e2eTests/InitializeTestParameters.ps1
+++ e2eTests/InitializeTestParameters.ps1
@@ -40,11 +40,17 @@ Set-ReleaseVariable "resourceGroupName" $($inputs.resourceGroupName)
 Set-ReleaseVariable "windowsVmName" $($inputs.windowsVmName)
 Set-ReleaseVariable "location" $($inputs.location)
 Set-ReleaseVariable "windowsVmStorageAccountName" $($inputs.windowsVmStorageAccountName)
-Set-ReleaseVariable "windowsVmTemplateFile" $(Join-Path $currentScriptPath $inputs.windowsVmTemplateFile)
-Set-ReleaseVariable "windowsVmTemplateParameterFile" $(Join-Path $currentScriptPath $inputs.windowsVmTemplateParameterFile)
+Set-ReleaseVariable "TemplateFile" $(Join-Path $currentScriptPath $inputs.TemplateFile)
+Set-ReleaseVariable "TemplateParameterFile" $(Join-Path $currentScriptPath $inputs.TemplateParameterFile)
 $windowsExtensionPublicSettingsFile = Join-Path $currentScriptPath $inputs.windowsExtensionPublicSettingsFile
 Set-ReleaseVariable "windowsExtensionName" $($inputs.windowsExtensionName)
 Set-ReleaseVariable "windowsExtensionPublisher" $($inputs.windowsExtensionPublisher)
+
+Set-ReleaseVariable "linuxVmName" $($inputs.linuxVmName)
+Set-ReleaseVariable "linuxVmStorageAccountName" $($inputs.linuxVmStorageAccountName)
+$linuxExtensionPublicSettingsFile = Join-Path $currentScriptPath $inputs.linuxExtensionPublicSettingsFile
+Set-ReleaseVariable "linuxExtensionName" $($inputs.linuxExtensionName)
+Set-ReleaseVariable "linuxExtensionPublisher" $($inputs.linuxExtensionPublisher)
 
 # get config settings
 $config = Get-Config -extensionPublicSettingsFile $windowsExtensionPublicSettingsFile
@@ -52,3 +58,5 @@ Set-ReleaseVariable "VSTSAccountName" $($config.VSTSUrl)
 Set-ReleaseVariable "TeamProject" $($config.TeamProject)
 Set-ReleaseVariable "MachineGroup" $($config.MachineGroup)
 Set-ReleaseVariable "WindowsAgentName" $($config.AgentName)
+$config = Get-Config -extensionPublicSettingsFile $linuxExtensionPublicSettingsFile
+Set-ReleaseVariable "LinuxAgentName" $($config.AgentName)
--- e2eTests/ValidateAgentOnRemoteTfat.ps1
+++ e2eTests/ValidateAgentOnRemoteTfat.ps1
@@ -6,23 +6,40 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$MachineGroup,
     [Parameter(Mandatory=$true)]
-    [string]$AgentName
+    [string[]]$AgentNames
 )
 
 . "$PSScriptRoot\VSTSAgentTestHelper.ps1"
 
-# Verify that agent is correctly configured against VSTS
-Write-Host "Validating that agent has been registered..."
-Write-Host "Getting agent information from VSTS"
-$agentInfo = Get-VSTSAgentInformation -vstsUrl "http://localhost:8080/tfs/defaultcollection" -teamProject $TeamProject -patToken $PATToken -machineGroup $MachineGroup -agentName $AgentName
-
-if(($agentInfo.isAgentExists -eq $false) -or ($agentInfo.isAgentOnline -eq $false))
-{
-    Write-Error "Agent has not been registered with VSTS!!"
+$AgentNames | % {
+    Confirm-AgentRegistered -TeamProject $TeamProject -PATToken $PATToken -MachineGroup -AgentName $_
 }
-else
-{
-    Write-Host "Agent has been successfully registered with VSTS!!"
 
-    #TODO: Remove agent from VSTS
+function Confirm-AgentRegistered
+{
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$TeamProject,
+        [Parameter(Mandatory=$true)]
+        [string]$PATToken,
+        [Parameter(Mandatory=$true)]
+        [string]$MachineGroup,
+        [Parameter(Mandatory=$true)]
+        [string]$AgentName
+    )
+
+    # Verify that agent is correctly configured against VSTS
+    Write-Host "Validating that agent $AgentName has been registered..."
+    Write-Host "Getting agent information from VSTS"
+
+    $agentInfo = Get-VSTSAgentInformation -vstsUrl "http://localhost:8080/tfs/defaultcollection" -teamProject $TeamProject -patToken $PATToken -machineGroup $MachineGroup -agentName $AgentName
+
+    if(($agentInfo.isAgentExists -eq $false) -or ($agentInfo.isAgentOnline -eq $false))
+    {
+        Write-Error "Agent $AgentName has not been registered with VSTS!!"
+    }
+    else
+    {
+        Write-Host "Agent $AgentName has been successfully registered with VSTS!!"
+    }
 }

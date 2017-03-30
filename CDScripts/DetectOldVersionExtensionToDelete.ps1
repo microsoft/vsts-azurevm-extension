57b5c81bbb138b687313895b64f130a783f960e6
Tejas Deshpande <tedeshpa@microsoft.com>
2017-03-30 15:07:30 +0530

    numeric sorting

--- CDScripts/DetectOldVersionExtensionToDelete.ps1
+++ CDScripts/DetectOldVersionExtensionToDelete.ps1
@@ -20,14 +20,14 @@ $versionToDelete = "NOTHING_TO_DELETE"
 # Fetching list of published extension handler. Using South Central US location. Since, we are insterested in oldest version, it does not matter which location we use as replication would have anyways completed.
 # The returned list is already sorted by oldest published version first
 $extensions = Get-AzureRmVMExtensionImage -Location southcentralus -PublisherName $publisher -Type $extensionName
-$extensions =  $extensions | Sort-Object -Property @{Expression = {$_.Version.Split(".")[0]}}, @{Expression = {$_.Version.Split(".")[1]}}
+$extensions =  $extensions | Sort-Object -Property @{Expression = {[int]$_.Version.Split(".")[0]}}, @{Expression = {[int]$_.Version.Split(".")[1]}}
 
 Write-Host "Published extension handler versions:"
 $extensions | % { Write-Host $_.version}
 
 if($extensions.Count -ge 5) 
 {
-    $versionToDelete = $extensions[1].Version
+    $versionToDelete = $extensions[0].Version
     Write-Host "5 or more extension handler versions are published. Oldest version can be deleted"
 }
 else

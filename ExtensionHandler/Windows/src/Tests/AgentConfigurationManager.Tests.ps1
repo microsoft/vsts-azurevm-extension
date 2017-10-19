f7085ecfdaa6b84a258c16f3059d9820f2d93089
Tejas Deshpande <tedeshpa@microsoft.com>
2017-10-20 01:35:22 +0530

    minor fix in ut

--- ExtensionHandler/Windows/src/Tests/AgentConfigurationManager.Tests.ps1
+++ ExtensionHandler/Windows/src/Tests/AgentConfigurationManager.Tests.ps1
@@ -6,7 +6,7 @@ Describe "Agent Configuration Manager Tests" {
     Context "Agent config command" {
 
         It "should quote arguments containing spaces" {
-            $expectedArgs = "--deploymentgroup --runasservice --unattended --auth PAT  --agent `"my agent`" --url `"https://acccount.visualstudio.com`" --token pat --work `"C:\work folder`" --projectname `"my proj`" --deploymentgroupname `"my dg`""
+            $expectedArgs = "--deploymentgroup --runasservice --unattended --replace --auth PAT  --agent `"my agent`" --url `"https://acccount.visualstudio.com`" --token pat --work `"C:\work folder`" --projectname `"my proj`" --deploymentgroupname `"my dg`""
             $ret = CreateConfigCmdArgs -tfsUrl "https://acccount.visualstudio.com" -patToken "pat" -workingFolder "C:\work folder" -projectName "my proj" -deploymentGroupName "my dg" -agentName "my agent"
             $ret | Should be $expectedArgs
         }

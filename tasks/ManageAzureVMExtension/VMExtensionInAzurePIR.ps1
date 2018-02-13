46f26399f87340a4df19cdaa6c381ec5e68e97a5
Tejas Deshpande <tedeshpa@microsoft.com>
2018-02-13 15:16:13 +0530

    cleaning up redundant code

--- tasks/ManageAzureVMExtension/VMExtensionInAzurePIR.ps1
+++ tasks/ManageAzureVMExtension/VMExtensionInAzurePIR.ps1
@@ -1,6 +1,10 @@
 Trace-VstsEnteringInvocation $MyInvocation
 Import-VstsLocStrings "$PSScriptRoot\Task.json"
 
+. $PSScriptRoot/StorageAccountOperations.ps1
+. $PSScriptRoot/AzurePIROperations.ps1
+. $PSScriptRoot/Utils.ps1
+
 # Get inputs.
 $connectedServiceName = Get-VstsInput -Name ConnectedServiceName -Require
 $action = Get-VstsInput -Name Action -Require
@@ -13,7 +17,7 @@ $extensionName = Get-VstsInput -Name ExtensionName -Require
 $publisherName = Get-VstsInput -Name Publisher -Require
 $extensionVersion = Get-VstsInput -Name Version -Require
 
-# Validate the script path and args do not contains new-lines. Otherwise, it will
+# Validate the extension definition file path does not contains new-lines. Otherwise, it will
 # break invoking the script via Invoke-Expression.
 if ($extensionDefinitionFilePath -match '[\r\n]' -or [string]::IsNullOrWhitespace($extensionDefinitionFilePath)) {
     throw (Get-VstsLocString -Key InvalidScriptPath0 -ArgumentList $extensionDefinitionFilePath)
@@ -21,7 +25,7 @@ if ($extensionDefinitionFilePath -match '[\r\n]' -or [string]::IsNullOrWhitespac
 
 # Initialize Azure.
 Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers_
-Initialize-Azure -azurePsVersion ""
+#Initialize-Azure -azurePsVersion ""
 
 function Get-ServiceEndpointDetails {
     param([string][Parameter(Mandatory = $true)]$connectedServiceName)
@@ -30,39 +34,7 @@ function Get-ServiceEndpointDetails {
     return $endpoint
 }
 
-function Upload-ExtensionPackageToStorageBlob {
-    param([string][Parameter(Mandatory = $true)]$storageAccountName,
-        [string][Parameter(Mandatory = $true)]$containerName,
-        [string][Parameter(Mandatory = $true)]$packagePath,
-        [string][Parameter(Mandatory = $true)]$storageBlobName)
-
-    try {
-        Get-AzureStorageAccount -StorageAccountName $storageAccountName -ErrorAction Stop
-    }
-    catch {
-        if ($_.Exception.Message.Contains("ResourceNotFound")) {
-            New-AzureStorageAccount -StorageAccountName $storageAccountName -Location "South Central US"
-        }
-    }
-
-    $key = Get-AzureStorageKey -StorageAccountName $storageAccountName
-    $ctx = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $key.Primary
-
-    try {
-        Get-AzureStorageContainer -Name $containerName -Context $ctx -ErrorAction Stop
-    }
-    catch {
-        if ($_.Exception.Message.Contains("Can not find the container")) {
-            New-AzureStorageContainer -Name $containerName -Context $ctx
-        }
-    }
-    
-    Set-AzureStorageBlobContent -Container $containerName -File $packagePath -Blob $storageBlobName -Context $ctx -Force
-}
-
-
-
-function Edit-ExtensionDefinitionXML {
+function Get-ExtensionDefinitionXML {
     param([string][Parameter(Mandatory = $true)]$extensionDefinitionFilePath,
         [string][Parameter(Mandatory = $true)]$mediaLink)
 
@@ -71,84 +43,44 @@ function Edit-ExtensionDefinitionXML {
     return $extensionFileDocument
 }
 
-function Create-ExtensionPackageInAzurePIR {
-    param([xml][Parameter(Mandatory = $true)]$bodyxml,
-        [X509Certificate2][Parameter(Mandatory = $true)]$certificate,
-        [string][Parameter(Mandatory = $true)]$subscriptionId)
+function Upload-ExtensionPackageToStorageBlob {
+    param([string][Parameter(Mandatory = $true)]$subscriptionId,
+        [string][Parameter(Mandatory = $true)]$storageAccountName,
+        [string][Parameter(Mandatory = $true)]$containerName,
+        [string][Parameter(Mandatory = $true)]$packagePath,
+        [string][Parameter(Mandatory = $true)]$storageBlobName,
+        [System.Security.Cryptography.X509Certificates.X509Certificate2][Parameter(Mandatory = $true)]$certificate)
 
-    Write-Host "Updating extension to version: $($bodyxml.ExtensionImage.Version)"
-
-    $uri = "https://management.core.windows.net/$subscriptionId/services/extensions"
-    Write-Host "uri: $uri"
-
-    # invoke POST rest api to create the extension
-    Invoke-RestMethod -Method POST -Uri $uri -Certificate $certificate -Headers @{'x-ms-version' = '2014-08-01'} -Body $bodyxml.OuterXml -ContentType application/xml
-
-    # set this version as value for release variable 
-    $newVersion = $bodyxml.ExtensionImage.Version
-    $newVersionVariable = "NewVersion"
-    Write-Host "##vso[task.setvariable variable=$newVersionVariable;]$newVersion"
-
-}
-
-function Invoke-WithRetry {
-    param (
-        [ScriptBlock] $retryCommand,
-        [int] $retryInterval = 120,
-        [int] $maxRetries = 60
-    )
-
-    $retryCount = 0
-    $isExecutedSuccessfully = $false
-
-    do {
-        try {
-            & $retryCommand
-            $isExecutedSuccessfully = $true
-            break
-        }
-        catch {
-            Write-Host "Exception code: $($_.Exception.Response.StatusCode.ToString())"
-            Write-Host $_
-
-            if ($_.Exception.Response.StatusCode.ToString() -ne "Conflict") {
-                Write-Error "Failed with non-conflict error. No need to retry. Fail now."
-                exit
-            }
-        }
-    
-        Write-Host "success: $isExecutedSuccessfully, retry count: $retryCount, max retries: $maxRetries. Will retry after $retryInterval seconds"
-        $retryCount++
-        Start-Sleep -s $retryInterval
-
+    try {
+        $uri = "https://management.core.windows.net/$subscriptionId/services/storageservices/$storageAccountName"
+        $result = Invoke-RestMethod -Method GET -Uri $uri -Certificate $certificate -Headers @{'x-ms-version' = '2014-08-01'}
     }
-    While (($isExecutedSuccessfully -ne $true) -and ($retryCount -lt $maxRetries))
-
-    if ($isExecutedSuccessfully -ne $true) {
-        Write-Error "Could not execute command successfully. Failing with timeout."
-        exit
+    catch {
+        if ($_.ErrorDetails.Message.Contains("ResourceNotFound")) {
+            Create-ClassicStorageAccount -subscriptionId $subscriptionId -storageAccountName $storageAccountName -certificate $certificate
+        }
+        else {
+            throw $_
+        }
     }
-}
 
-function Update-ExtensionPackageInAzurePIR {
-    param([xml][Parameter(Mandatory = $true)]$bodyxml,
-        [System.Security.Cryptography.X509Certificates.X509Certificate2][Parameter(Mandatory = $true)]$certificate,
-        [string][Parameter(Mandatory = $true)]$subscriptionId)
-
-    $certificate.Thumbprint
-    Write-Host "Updating extension to version: $($bodyxml.ExtensionImage.Version)"
-
-    $uri = "https://management.core.windows.net/$subscriptionId/services/extensions?action=update"
-    Write-Host "uri: $uri"
-
-    # invoke PUT rest api to update the extension
+    $keysUri = "https://management.core.windows.net/$subscriptionId/services/storageservices/$storageAccountName/keys"
+    $keysResult = Invoke-RestMethod -Method GET -Uri $keysUri -Certificate $certificate -Headers @{'x-ms-version' = '2014-08-01'}
+    $storageAccountKey = $keysResult.StorageService.StorageServiceKeys.Primary
     
-    Invoke-WithRetry -retryCommand {Invoke-RestMethod -Method PUT -Uri $uri -Certificate $certificate -Headers @{'x-ms-version' = '2014-08-01'} -Body $bodyxml.OuterXml -ContentType application/xml -ErrorAction SilentlyContinue}
-           
-    # set this version as value for release variable 
-    $newVersion = $bodyxml.ExtensionImage.Version
-    $newVersionVariable = "NewVersion"
-    Write-Host "##vso[task.setvariable variable=$newVersionVariable;]$newVersion"
+    try {
+        Get-ContainerDetails -storageAccountName $storageAccountName -containerName $containerName -storageAccountKey $storageAccountKey
+    }
+    catch {
+        if ($_.ErrorDetails.Message.Contains("ContainerNotFound")) {
+            Create-NewContainer -storageAccountName $storageAccountName -containerName $containerName -storageAccountKey $storageAccountKey
+        }
+        else {
+            throw $_
+        }
+    }
+    
+    Set-StorageBlobContent -storageAccountName $storageAccountName -containerName $containerName -storageBlobName $storageBlobName -packagePath $packagePath -storageAccountKey $storageAccountKey
 }
 
 function Upload-ExtensionPackageToAzurePIR {
@@ -162,60 +94,20 @@ function Upload-ExtensionPackageToAzurePIR {
 
     # read extension definition
     $mediaLink = "https://{0}.blob.core.windows.net/{1}/{2}" -f $storageAccountName, $containerName, $storageBlobName
-    $bodyxml = Edit-ExtensionDefinitionXML -extensionDefinitionFilePath $extensionDefinitionFilePath -mediaLink $mediaLink
+    $bodyxml = Get-ExtensionDefinitionXML -extensionDefinitionFilePath $extensionDefinitionFilePath -mediaLink $mediaLink
     Write-Host "Body xml: $bodyxml"
-
-    if ($extensionVersion -eq "1.0.0.0") {
-        Create-ExtensionPackageInAzurePIR -bodyxml $bodyxml -certificate $certificate -subscriptionId $subscriptionId
-    }
-    else {
+    
+    $extensionExistsInPIR = $false
+    $extensionExistsInPIR = Check-ExtensionExistsInAzurePIR -subscriptionId $subscriptionId -certificate $certificate -publisher $bodyxml.ExtensionImage.ProviderNameSpace -type $bodyxml.ExtensionImage.Type
+    if ($extensionExistsInPIR) {
         Update-ExtensionPackageInAzurePIR -bodyxml $bodyxml -certificate $certificate -subscriptionId $subscriptionId
     }
-}
-
-function Delete-ExtensionPackageFromAzurePIR {
-    param([string][Parameter(Mandatory = $true)]$extensionName,
-        [string][Parameter(Mandatory = $true)]$publisher,
-        [string][Parameter(Mandatory = $true)]$versionToDelete,
-        [System.Security.Cryptography.X509Certificates.X509Certificate2][Parameter(Mandatory = $true)]$certificate,
-        [string][Parameter(Mandatory = $true)]$subscriptionId)
-
-    if ($versionToDelete -eq "NOTHING_TO_DELETE") {
-        Write-Host "No extension will be deleted"
-        return
+    else {
+        $bodyxml.ExtensionImage.Version = "1.0.0.0"
+        Create-ExtensionPackageInAzurePIR -bodyxml $bodyxml -certificate $certificate -subscriptionId $subscriptionId
     }
-
-    Write-Host "Deleting extension version: $versionToDelete"
-    $certificate.Thumbprint
-
-    # First set extension as internal and then delete
-    [xml]$definitionXml = [xml]('<?xml version="1.0" encoding="utf-8"?>
-  <ExtensionImage xmlns="http://schemas.microsoft.com/windowsazure" xmlns:i="http://www.w3.org/2001/XMLSchema-instance">   
-  <ProviderNameSpace></ProviderNameSpace>
-  <Type></Type>
-  <Version></Version>
-  <IsInternalExtension>true</IsInternalExtension>
-  <IsJsonExtension>true</IsJsonExtension>
-  </ExtensionImage>')
-
-    $definitionXml.ExtensionImage.ProviderNameSpace = [string]$publisher
-    $definitionXml.ExtensionImage.Type = [string]$extensionName
-    $definitionXml.ExtensionImage.Version = [string]$versionToDelete
-    $($definitionXml.ExtensionImage.version)
-
-    $putUri = "https://management.core.windows.net/$subscriptionId/services/extensions?action=update"
-    Write-Host "Updating extension to mark it as internal. using uri: $putUri"
-
-    Invoke-WithRetry -retryCommand { Invoke-RestMethod -Method PUT -Uri $putUri -Certificate $certificate -Headers @{'x-ms-version' = '2014-08-01'} -Body $definitionXml -ContentType application/xml -ErrorAction SilentlyContinue }
-
-    Start-Sleep -Seconds 10
-
-    # now delete
-    $uri = "https://management.core.windows.net/$subscriptionId/services/extensions/$publisher/$extensionName/$versionToDelete"
-    Write-Host "Deleting extension. using uri: $uri"
-
-    Invoke-WithRetry -retryCommand { Invoke-RestMethod -Method DELETE -Uri $uri -Certificate $certificate -Headers @{'x-ms-version' = '2014-08-01'} -ErrorAction SilentlyContinue }
 }
+
 
 $serviceEndpointDetails = Get-ServiceEndpointDetails -connectedServiceName $connectedServiceName
 
@@ -224,7 +116,7 @@ $certificate = New-Object System.Security.Cryptography.X509Certificates.X509Cert
 $certificate.Import($bytes, $null, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
 
 if ($action -eq "Upload") {
-    Upload-ExtensionPackageToStorageBlob -storageAccountName $storageAccountName -containerName $containerName -packagePath $extensionPackagePath -storageBlobName $storageBlobName
+    Upload-ExtensionPackageToStorageBlob -subscriptionId $serviceEndpointDetails.Data.subscriptionId -storageAccountName $storageAccountName -containerName $containerName -packagePath $extensionPackagePath -storageBlobName $storageBlobName -certificate $certificate
     Upload-ExtensionPackageToAzurePIR -subscriptionId $serviceEndpointDetails.Data.subscriptionId -storageAccountName $storageAccountName -containerName $containerName -storageBlobName $storageBlobName -extensionDefinitionFilePath $extensionDefinitionFilePath -certificate $certificate -extensionVersion $extensionVersion
 }
 elseif ($action -eq "Delete") {

4ca4322fc9b7f4bdaf7ccb1cc54f45d5835af178
Tejas Deshpande <tedeshpa@microsoft.com>
2019-02-19 10:54:55 +0530

    adding logic to skip deleting specified versions in script

--- CDScripts/DetectOldVersionExtensionToDelete.ps1
+++ CDScripts/DetectOldVersionExtensionToDelete.ps1
@@ -1,18 +1,20 @@
 <#
 .Synopsis
     Detects the oldest version of published extension and sets a release variable value
+    Has an optional parameter to skip deleting specific version, accepted as input in semicolon separated form. 
 
 .Usage
-    DetectOldVersionExtensionToDelete.ps1 -extensionName ReleaseManagement1 -publisher Test.Microsoft.VisualStudio.Services -versionToDelete 1.9.0.0
+    DetectOldVersionExtensionToDelete.ps1 -extensionName ReleaseManagement1 -publisher Test.Microsoft.VisualStudio.Services -versionToDelete 1.9.0.0 -versionsToSkip "1.1.0.0; 1.2.0.0"
 #>
 
 [CmdletBinding()]
 param(
     [Parameter(Mandatory=$true)]
     [string]$extensionName,
-
     [Parameter(Mandatory=$true)]
-    [string]$publisher
+    [string]$publisher,
+    [Parameter(Mandatory=$false)]
+    [string]$versionsToSkip
 )
 
 $versionToDelete = "NOTHING_TO_DELETE"
@@ -20,6 +22,12 @@ $versionToDelete = "NOTHING_TO_DELETE"
 # Fetching list of published extension handler. Using South Central US location. Since, we are insterested in oldest version, it does not matter which location we use as replication would have anyways completed.
 # The returned list is already sorted by oldest published version first
 $extensions = Get-AzureRmVMExtensionImage -Location southcentralus -PublisherName $publisher -Type $extensionName
+if($versionsToSkip.Trim())
+{
+    $versionsToSkipArray = $versionsToSkip.Split(";") | %{$_.Trim()}
+    Write-Host "Will skip deleting the following versions: $versionsToSkipArray"
+    $extensions = $extensions | Where-Object {!$versionsToSkipArray.Contains($_.Version)}
+}
 $extensions =  $extensions | Sort-Object -Property @{Expression = {[int]$_.Version.Split(".")[0]}}, @{Expression = {[int]$_.Version.Split(".")[1]}}
 
 Write-Host "Published extension handler versions:"

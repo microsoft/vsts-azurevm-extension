27a48a5167aab82e9f355575da254c399d167d57
Pranshu-Negi <pranshunegi@microsoft.com>
2023-03-29 10:25:45 +0200

    Add -UseBasicParsing parameter in webrequest

--- ExtensionHandler/Windows/src/bin/RMExtensionUtilities.ps1
+++ ExtensionHandler/Windows/src/bin/RMExtensionUtilities.ps1
@@ -229,7 +229,7 @@ function DoesSystemPersistsInNet6Whitelist {
 
     $WindowsVersion = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").CurrentBuildNumber
 
-    $Net6SupportedOS = Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure-pipelines-agent/master/src/Agent.Listener/net6.json" | ConvertFrom-Json
+    $Net6SupportedOS = Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure-pipelines-agent/master/src/Agent.Listener/net6.json" -UseBasicParsing | ConvertFrom-Json
 
     foreach ($supportedOS in $Net6SupportedOS)
     {

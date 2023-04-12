c8e5d565d178e6cb22c1eeae6a91e25b7cffd852
Pranshu-Negi <pranshunegi@microsoft.com>
2023-04-12 10:01:44 +0200

    fix test

--- ExtensionHandler/Windows/src/Tests/EnsureNet6FileConsistent.Tests.ps1
+++ ExtensionHandler/Windows/src/Tests/EnsureNet6FileConsistent.Tests.ps1
@@ -4,7 +4,8 @@ Describe "Ensure Net6 File Consistent Tests" {
         It "should verify that local copy of net6.json is same as the original file maintained in the azure-pipelines-agent repo " {
             $local = (Get-Content "..\net6.json" -Raw) | ConvertFrom-Json
             $remote = Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure-pipelines-agent/master/src/Agent.Listener/net6.json" -UseBasicParsing | ConvertFrom-Json
-            $local | Should -Be $remote
+            $diff = Compare-Object -ReferenceObject $local -DifferenceObject $remote
+            $diff -eq $null | Should -Be $true
         }
     }
 }

0a217b47ee52baef7cf7e2407ffe7a7e97908b80
Pranshu-Negi <pranshunegi@microsoft.com>
2023-04-12 09:39:15 +0200

    Add check for consistency of net6.json file

---
+++ ExtensionHandler/Windows/src/Tests/EnsureNet6FileConsistent.Tests.ps1
@@ -0,0 +1,10 @@
+Describe "Ensure Net6 File Consistent Tests" {
+
+    Context "The local copy of the net6.json file should be consistent with the original maintained in the azure-pipelines-agent repo" {
+        It "should verify that local copy of net6.json is same as the original file maintained in the azure-pipelines-agent repo " {
+            $local = (Get-Content "..\net6.json" -Raw) | ConvertFrom-Json
+            $remote = Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure-pipelines-agent/master/src/Agent.Listener/net6.json" -UseBasicParsing | ConvertFrom-Json
+            $local | Should -Be $remote
+        }
+    }
+}
